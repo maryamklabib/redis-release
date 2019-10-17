a9c9d044e1dac2cefc667bc286e116b7d911ca47
Maryam Labib <mlabib@pivotal.io>
2019-10-17 11:58:59 -0700

    Create a Redis BOSH release
    [#169158815]
    
    Co-authored-by: Maryam Labib <mlabib@pivotal.io>
    Co-authored-by: Michael Edoror <medoror@pivotal.io>

---
+++ jobs/redis/templates/ctl.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+RUN_DIR=/var/vcap/sys/run/redis
+LOG_DIR=/var/vcap/sys/log/redis
+
+case $1 in
+
+  start)
+    mkdir -p $RUN_DIR $LOG_DIR
+    chown -R vcap:vcap $RUN_DIR $LOG_DIR
+
+    /var/vcap/packages/redis/bin/redis-server \
+    /var/vcap/jobs/redis/redis.conf \
+    1>> $LOG_DIR/redis.out \
+    2>> $LOG_DIR/redis.err
+
+    ;;
+
+  stop)
+    /var/vcap/packages/redis/bin/redis-cli shutdown
+
+    ;;
+
+  *)
+    echo "Usage: ctl {start|stop}" ;;
+
+esac
